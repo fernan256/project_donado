e5b98b0b81835a1658577904d788a4072609ae02
Diego Mayorga <diego.mayorga86@gmail.com>
2019-04-14 20:11:56 -0300

    (index): added headers, decoder and method

--- restful_api/index.js
+++ restful_api/index.js
@@ -5,23 +5,44 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all request with a string
 var server = http.createServer(function(req, res) {
 	
 	// Get the URL and parse it
-	var parseURL = url.parse(req.url,trur);
+	var parsedURL = url.parse(req.url,true);
 
 	// Ger the path
-	var path = parseURL.pathName;
+	var path = parsedURL.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+	// Get the query string as a object
+	var queryStringObject = parsedURL.query;
 
-	// Send the response
-	res.end('Hello diego\n');
+	// Get the http method
+	var method = req.method.toLowerCase();
 
-	// Log the request path
+	// Get the headers as an object
+	var headers = req.headers;
 
+	// Get the payload, if any
+	var decoder = new StringDecoder('utf-8');
+	var buffer = '';
+	req.on('data',function(data) {
+		buffer += decoder.write(data);
+	});
+	req.on('end',function() {
+		buffer += decoder.end();
 
+		// Send the response
+		res.end('Hello diego\n');
+
+		// Log the request path
+		console.log('Request received on path: '+trimmedPath+ ' with this method: '+method+ ' and this query: ',queryStringObject);
+		console.log('Headers: ', headers);
+		console.log('This payload: ', buffer);
+	});
 });
 
 // Start the server and have it listen on port 3000
