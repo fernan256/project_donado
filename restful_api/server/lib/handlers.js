f10a967f86a4ca4a3a397680bd30a19b069d4df6
Diego Mayorga <diego.mayorga86@gmail.com>
2019-04-20 21:37:43 -0300

    Add put and delete handlers

--- restful_api/server/lib/handlers.js
+++ restful_api/server/lib/handlers.js
@@ -77,7 +77,7 @@ handlers._users.post = (data, callback) => {
 // Users - get
 // Required data: phone
 // Optional data: none
-// @TODO Only let an authenticated user acces their object.
+// @TODO Only let an authenticated user access their object.
 handlers._users.get = (data, callback) => {
   // Check that the phone number is valid
   let phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
@@ -99,12 +99,84 @@ handlers._users.get = (data, callback) => {
 }
 
 // Users - put
+// Required data: phone
+// Optional data: firstName, lastName, password (at least must be specified)
+// @TODO Only let an authenticated user update their object.
 handlers._users.put = (data, callback) => {
+  // Check that the phone number is valid
+  let phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
 
+  // Check optional fields
+  let firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+  let lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+  let password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+
+  // Error if the phone is invalid
+  if(phone) {
+    // Error if nothing is sent to update
+    if(firstName || lastName || password) {
+      // Lookup the user
+      _data.read('users',phone, (err,userData) => {
+        if(!err && userData) {
+          // update the fields necessary
+          if(firstName) {
+            userData.firstName = firstName;
+          }
+          if(lastName) {
+            userData.lastName = lastName;
+          }
+          if(password) {
+            userData.hashedPassword = helpers.hash(password);
+          }
+          // Store the updates
+          _data.update('users', phone, userData, (err,) => {
+            if(!err) {
+              callback(200);
+            } else {
+              console.log(err);
+              callback(500, {'Error' : 'Could not update the user'});
+            }
+          });
+        } else {
+          callback(400, {'Error' : 'The specified user does not exist'});
+        }
+      })
+
+    } else {
+      callback(400, {'Error' : 'Missing fields to update'});
+    }
+  } else {
+    callback(400, {'Error' : 'Missing required field'});
+  }
 }
 
 // Users - delete
+// Required data: phone
+// @TODO Only let an authenticated user update their object.
+// @TODO Cleanup (delete) any other data files associated with this user.
 handlers._users.delete = (data, callback) => {
+  // Check that the phone number is valid
+  let phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+
+  if(phone) {
+    // Lookup the user
+    _data.read('users',phone, (err,data) => {
+      if(!err && data) {
+        // Remove password before return callback
+        _data.delete('users', phone, (err) => {
+          if(!err) {
+            callback(200);
+          } else {
+            callback(500, {'Error' : 'Could not delete the specified user'});
+          }
+        })
+      } else {
+        callback(400, {'Error': 'Could not find the specified user'});
+      }
+    })
+  } else {
+    callback(400, {'Error' : 'Missing required field'});
+  }
 
 }
 
